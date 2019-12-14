06beca6e83ba75716abc986f043a693408a4b7cd
Ahernandezl0889 <ahernandezl0889@gmail.com>
2019-12-14 12:56:27 -0500

    new changes

--- js/main.js
+++ js/main.js
@@ -10,4 +10,34 @@ function initMap() {
     const marker = new google.maps.Marker({ position: loc, map: map });
   }
   
+// sticky navbar
+window.addEventListener('scroll', function(){
+  if (window.scrollY > 150) {
 
+    document.querySelector('#navbar').style.opacity = 0.9;
+
+  } else {
+    document.querySelector('navbar').style.opacity = 1;
+  }
+})
+
+
+  // Smooth Scrolling 
+  // target the navbar link
+  $('#navbar a, .btn').on('click',function(event){
+     
+    // checking the hash
+    if (this.hash !== '') {
+      event.preventDefault();
+     
+      const hash = this.hash;
+
+      $("html, body").animate(
+        {
+        scrollTop: $(hash).offset().top -100
+              // speed
+      }, 800
+      );
+    }
+  });
+ 
